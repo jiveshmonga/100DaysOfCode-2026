6db97d601c25ac3db6abee414d67819f2960e03f
Drishti Seth <drishtiseth4293@gmail.com>
2026-06-17 22:25:33 +0530

    Update Question1.py

--- DSA/DrishtiSeth_590011761/Day_2/Question1.py
+++ DSA/DrishtiSeth_590011761/Day_2/Question1.py
@@ -1,2 +1,10 @@
-arr = list(map(int, input().split()))
-print(sum(arr))
+class Solution:
+    def removeElement(self, nums, val):
+        k = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
